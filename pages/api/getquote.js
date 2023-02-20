a4bccaf15d95288d04c061bbb964fc5a842f4f94
Martin Šíl <martin.sil1337@gmail.com>
2023-02-20 22:50:31 +0100

    Popis toho co kód dělá

--- pages/api/getquote.js
+++ pages/api/getquote.js
@@ -1,19 +1,33 @@
+// Importuje se exportovaná funkce ze souboru s propojením OpenAI
 import { openai } from '@/utils/openai';
 
 export default async function handler(req, res) {
+	// Pokud metoda je jiná než POST, vrátí se error
 	if (req.method === 'POST') {
+		// Do data se deklaruje obsah v těle requestu
 		const data = req.body;
+
+		// Pokud chybí data vrátí se error
 		if (!data) {
 			res.status(400).json({ error: 'Body is missing' });
 			return;
 		}
+		// Pokud chybí input vrátí se error
 		if (!data.input) {
 			res.status(400).json({ error: 'Data is missing' });
 			return;
 		}
 
-		var text = `Create product description for a mug that cannot spill the water.`;
+		// Vytvoří se proměnná pro promt, který se poté posílá OpenAI, 
+		// pokud chceme udělat string ve kterém se nachází zakomponovaný input,
+		// co nám poslal uživatel z frontendu musí se místo klasických uvozovek použít backticky
+		// a slova se vnořují dovnitř pomocí dollaru a závorek - ${data.input}
+		var text = `I want you to act as a Jordan Peterson and your job is to make some short quote about life experience that could motivate person doing ${data.input}.`;
 		
+		// Teď se pokusí zavolat na OpenAI API s definovaným nastavením
+		// Pokud uspěje, ale navrátí se nic vrátí error
+		// Pokud uspěje a vrátí něco, tato honota se pošle na frontend
+		// Pokud to trvá moc dlouho, nebo nastane error, vrátí error
 		try {
 			const aires = await openai.createCompletion({
 				model: 'text-davinci-003',
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
+// Aby se mohl use state používat, tak se importne z balíčku do kterého patří
 import { useState } from 'react';
 
 export default function Home() {
+  // Jedná se o deklaraci stavové proměnné React pomocí useState "hooku"
+  // s počáteční hodnotou prázdného stringu. K aktualizaci její hodnoty 
+  // lze použít funkci setInputValue.
   const [inputValue, setInputValue] = useState('');
 
+  // Tato funkce se vyvolá při odeslání formuláře
+  // Spouští se dole ve formě pomocí <form onSubmit={handleSubmit}>
   const handleSubmit = async (e) => {
+    // Toto zabrání refreshování stránky při odeslání formuláře
+    // Samozřejmě se do funkce musí poslat (e)
     e.preventDefault();
+
+    //Definuje se tělo requestu pomocí objektu
     const body = {
       input: inputValue,
     }
+
+    // Zavolá se metodou POST na url /api/getquote,
+    // které se řekne že se posílá obsah typu JSON 
+    // a pošle se body (Které se převede na JSON)
     const res = await fetch("/api/getquote", {
       method: "POST",
       headers: {
@@ -15,7 +29,14 @@ export default function Home() {
       },
       body: JSON.stringify(body)
     })
+
+    // Samotná funkce "conts res" se neprovede a abychom
+    // z ní vytěžili odpověď tak jí musíme vyvolat tím že
+    // do const quote uložíme odpověď v jsonu, to znamená,
+    // že se funkce vyvolá
     const quote = res.json();
+
+    // Poté se vypíše do konzole výsledek
     console.log(quote);
   }
 
